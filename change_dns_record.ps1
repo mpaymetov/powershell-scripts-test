fde1bbaa1bbf721bdb6d0b347b0db1e94c1d5529
Mihail Paimetov <mihail.paimetov@travelline.ru>
2021-08-16 17:36:36 +0300

    исправил адрес

--- change_dns_record.ps1
+++ change_dns_record.ps1
@@ -8,5 +8,5 @@ $Cred = New-Object -TypeName System.Management.Automation.PSCredential -Argument
 
 $ADServer = "main.test.local"
 Invoke-Command -ComputerName $ADServer -Credential $Cred -ScriptBlock {
-	Add-DnsServerResourceRecordCName -Name "webpms" -HostNameAlias "$DnsServerResourceRecordCName" -ZoneName "travelline.lan"
+	Add-DnsServerResourceRecordCName -Name "webpms" -HostNameAlias "$DnsServerResourceRecordCName" -ZoneName "test.local"
 }
